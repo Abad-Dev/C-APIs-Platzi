0aed1a51d975d795796029d08510f5c72a92ff14
sedron <jfabianabadd@gmail.com>
2023-11-30 11:03:28 -0500

    Created Program.cs

---
+++ Program.cs
@@ -0,0 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using webapi;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<TareasContext>(options => options.UseMySQL(""));
+//builder.Services.AddScoped<IHelloWorldService, HelloWorldService>();
+builder.Services.AddScoped<IHelloWorldService>(p=> new HelloWorldService());
+builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<ITareaService, TareaService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+// Middlewares:
+//app.UseWelcomePage();
+app.UseTimeMiddlware();
+
+app.MapControllers();
+
+app.Run();
